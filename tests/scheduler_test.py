500d73d8d1d2570f6a76d103b707abfa435fc322
over <overln2@gmail.com>
2015-11-23 12:32:15 +0600

    some tests for Scheduler

--- tests/scheduler_test.py
+++ tests/scheduler_test.py
@@ -5,19 +5,22 @@ from viewer import Scheduler
 import viewer
 from settings import settings
 from datetime import datetime
-from datetime import timedelta
 import assets_helper
 import db
 from random import Random
 from random import shuffle
+
+TIME_WHERE_X_Y_ACTIVE = datetime(2015, 11, 26)
+TIME_WHERE_Y_ACTIVE = datetime(2015, 11, 28)
+TIME_WHERE_NO_ACTIVE = datetime(2015, 11, 29)
 
 asset_x = {
     'mimetype': u'web',
     'asset_id': u'4c8dbce552edb5812d3a866cfe5f159d',
     'name': u'WireLoad',
     'uri': u'http://www.wireload.net',
-    'start_date': datetime.utcnow() - timedelta(days=2),
-    'end_date': datetime.utcnow() + timedelta(days=1),
+    'start_date': datetime(2015, 11, 23),
+    'end_date': datetime(2015, 11, 27),
     'duration': u'5',
     'is_enabled': 1,
     'nocache': 0,
@@ -42,8 +45,8 @@ asset_y = {
     'asset_id': u'7e978f8c1204a6f70770a1eb54a76e9b',
     'name': u'Google',
     'uri': u'https://www.google.com/images/srpr/logo3w.png',
-    'start_date': datetime.utcnow() - timedelta(days=1),
-    'end_date': datetime.utcnow() + timedelta(days=2),
+    'start_date': datetime(2015, 11, 25),
+    'end_date': datetime(2015, 11, 29),
     'duration': u'6',
     'is_enabled': 1,
     'nocache': 0,
@@ -67,15 +70,18 @@ asset_y_inactive = {
 class TestScheduler(unittest.TestCase):
     def setUp(self):
         viewer.HOME = '/tmp'
-        self.db_file = '/tmp/test.db'
 
-        if os.path.exists(self.db_file):
-            os.remove(self.db_file)
+        self.set_now(TIME_WHERE_X_Y_ACTIVE)
 
         settings.set({
             'main': {'database': 'test.db'},
             'viewer': {'shuffle_playlist': False}
         })
+
+        self.db_file = '/tmp/test.db'
+
+        if os.path.exists(self.db_file):
+            os.remove(self.db_file)
 
         self.conn = db.conn(self.db_file)
         viewer.db_conn = self.conn
@@ -86,6 +92,14 @@ class TestScheduler(unittest.TestCase):
 
     def tearDown(self):
         self.conn.close()
+        viewer.get_time = datetime.utcnow
+        assets_helper.get_time = datetime.utcnow
+
+        settings['shuffle_playlist'] = False
+
+    def set_now(self, t):
+        viewer.get_time = lambda: t
+        assets_helper.get_time = lambda: t
 
     def test_db_mtime(self):
         open(self.db_file, 'w').close()
@@ -97,13 +111,13 @@ class TestScheduler(unittest.TestCase):
         self.assertEqual(Scheduler.generate_asset_list(), ([], None))
 
         assets_helper.create_multiple(self.conn, [asset_x, asset_x_inactive, asset_y, asset_y_inactive])
-        self.assertEqual(Scheduler.generate_asset_list(), ([asset_y, asset_x], asset_y['end_date']))
+        self.assertEqual(Scheduler.generate_asset_list(), ([asset_y, asset_x], asset_x['end_date']))
 
     def test_get_next_asset_on_empty_db_should_return_none(self):
         self.assertEqual(self.scheduler.get_next_asset(), None)
 
     def test_get_next_asset_should_update_pl_on_db_change(self):
-        self.assertEqual(self.scheduler.get_next_asset(), None)
+        self.scheduler.refresh_playlist()
 
         assets_helper.create_multiple(self.conn, [asset_x])
         self.assertEqual(self.scheduler.get_next_asset(), asset_x)
@@ -112,128 +126,127 @@ class TestScheduler(unittest.TestCase):
         assets_helper.create_multiple(self.conn, [asset_x, asset_y])
         self.scheduler.refresh_playlist()
 
-        viewer.get_time = lambda: datetime.utcnow() + timedelta(days=10)
-        assets_helper.get_time = lambda: datetime.utcnow() + timedelta(days=10)
+        self.set_now(TIME_WHERE_Y_ACTIVE)
 
-        self.assertEqual(self.scheduler.get_next_asset(), None)
+        self.assertEqual(asset_y, self.scheduler.get_next_asset())
 
-    # def test_update_pl(self):
-    #     settings['shuffle_playlist'] = False
-    #     assets_helper.create(self.conn, asset_x)
-    #
-    #     self.assertEquals(([asset_x], 0), Scheduler.update_pl([], 0))
-    #     self.assertEquals(([asset_x], 0), Scheduler.update_pl([asset_x, asset_y], 1))
-    #     self.assertEquals(([asset_x], 0), Scheduler.update_pl([asset_x], 0))
-    #
-    # def test_update_pl_shuffled(self):
-    #     settings['shuffle_playlist'] = True
-    #     rnd = Random().random()
-    #
-    #     assets_helper.create_multiple(self.conn, [asset_x, asset_y])
-    #     assets, _ = Scheduler.update_pl(assets=[], index=0, shuffle_rnd=lambda: rnd)
-    #
-    #     shuffled = [asset_y, asset_x]
-    #     shuffle(shuffled, lambda: rnd)
-    #
-    #     self.assertEquals(shuffled, assets)
-    #
-    # def test_take_next(self):
-    #     # empty assets
-    #     self.assertEqual(Scheduler.take([], 0), (None, 0))
-    #
-    #     # one active asset
-    #     self.assertEqual(Scheduler.take([asset_x], 0), (asset_x, 0))
-    #
-    #     # one inactive asset
-    #     self.assertEqual(Scheduler.take([asset_x_inactive], 0), (None, 0))
-    #
-    #     # two active
-    #     self.assertEqual(Scheduler.take([asset_x, asset_y], 0), (asset_x, 1))
-    #     self.assertEqual(Scheduler.take([asset_x, asset_y], 1), (asset_y, 0))
-    #
-    #     # inactive, active
-    #     self.assertEqual(Scheduler.take([asset_x_inactive, asset_y], 0), (asset_y, 0))
-    #     self.assertEqual(Scheduler.take([asset_x_inactive, asset_y], 1), (asset_y, 0))
-    #
-    #     # active, inactive
-    #     self.assertEqual(Scheduler.take([asset_x, asset_y_inactive], 0), (asset_x, 1))
-    #     self.assertEqual(Scheduler.take([asset_x, asset_y_inactive], 1), (asset_x, 1))
-    #
-    #     # 2 inactive
-    #     self.assertEqual(Scheduler.take([asset_x_inactive, asset_y_inactive], 0), (None, 0))
-    #     self.assertEqual(Scheduler.take([asset_x_inactive, asset_y_inactive], 1), (None, 0))
-    #
-    # def test_shuffle_correct_repeat_detection(self):
-    #     self.assertEqual(Scheduler.shuffle_if_need([asset_x, asset_y], index=0, old_index=0, repeated=0), 1)
-    #     self.assertEqual(Scheduler.shuffle_if_need([asset_x, asset_y], index=0, old_index=1, repeated=0), 1)
-    #     self.assertEqual(Scheduler.shuffle_if_need([asset_x, asset_y], index=1, old_index=0, repeated=0), 0)
-    #
-    # def test_take_should_invoke_shuffle_after_MAX_REPEAT_reached(self):
-    #     settings['shuffle_playlist'] = True
-    #     viewer.SHUFFLE_MAX_REPEAT = 2
-    #     pl_orig = [asset_x, asset_y]
-    #     pl = [asset_x, asset_y]
-    #     rnd = Random().random()
-    #
-    #     self.assertEqual(Scheduler.shuffle_if_need(pl, index=0, old_index=0, repeated=2, shuffle_rnd=lambda: rnd), 0)
-    #     shuffle(pl_orig, lambda: rnd)
-    #
-    #     self.assertEqual(pl, pl_orig)
+        # def test_update_pl(self):
+        #     settings['shuffle_playlist'] = False
+        #     assets_helper.create(self.conn, asset_x)
+        #
+        #     self.assertEquals(([asset_x], 0), Scheduler.update_pl([], 0))
+        #     self.assertEquals(([asset_x], 0), Scheduler.update_pl([asset_x, asset_y], 1))
+        #     self.assertEquals(([asset_x], 0), Scheduler.update_pl([asset_x], 0))
+        #
+        # def test_update_pl_shuffled(self):
+        #     settings['shuffle_playlist'] = True
+        #     rnd = Random().random()
+        #
+        #     assets_helper.create_multiple(self.conn, [asset_x, asset_y])
+        #     assets, _ = Scheduler.update_pl(assets=[], index=0, shuffle_rnd=lambda: rnd)
+        #
+        #     shuffled = [asset_y, asset_x]
+        #     shuffle(shuffled, lambda: rnd)
+        #
+        #     self.assertEquals(shuffled, assets)
+        #
+        # def test_take_next(self):
+        #     # empty assets
+        #     self.assertEqual(Scheduler.take([], 0), (None, 0))
+        #
+        #     # one active asset
+        #     self.assertEqual(Scheduler.take([asset_x], 0), (asset_x, 0))
+        #
+        #     # one inactive asset
+        #     self.assertEqual(Scheduler.take([asset_x_inactive], 0), (None, 0))
+        #
+        #     # two active
+        #     self.assertEqual(Scheduler.take([asset_x, asset_y], 0), (asset_x, 1))
+        #     self.assertEqual(Scheduler.take([asset_x, asset_y], 1), (asset_y, 0))
+        #
+        #     # inactive, active
+        #     self.assertEqual(Scheduler.take([asset_x_inactive, asset_y], 0), (asset_y, 0))
+        #     self.assertEqual(Scheduler.take([asset_x_inactive, asset_y], 1), (asset_y, 0))
+        #
+        #     # active, inactive
+        #     self.assertEqual(Scheduler.take([asset_x, asset_y_inactive], 0), (asset_x, 1))
+        #     self.assertEqual(Scheduler.take([asset_x, asset_y_inactive], 1), (asset_x, 1))
+        #
+        #     # 2 inactive
+        #     self.assertEqual(Scheduler.take([asset_x_inactive, asset_y_inactive], 0), (None, 0))
+        #     self.assertEqual(Scheduler.take([asset_x_inactive, asset_y_inactive], 1), (None, 0))
+        #
+        # def test_shuffle_correct_repeat_detection(self):
+        #     self.assertEqual(Scheduler.shuffle_if_need([asset_x, asset_y], index=0, old_index=0, repeated=0), 1)
+        #     self.assertEqual(Scheduler.shuffle_if_need([asset_x, asset_y], index=0, old_index=1, repeated=0), 1)
+        #     self.assertEqual(Scheduler.shuffle_if_need([asset_x, asset_y], index=1, old_index=0, repeated=0), 0)
+        #
+        # def test_take_should_invoke_shuffle_after_MAX_REPEAT_reached(self):
+        #     settings['shuffle_playlist'] = True
+        #     viewer.SHUFFLE_MAX_REPEAT = 2
+        #     pl_orig = [asset_x, asset_y]
+        #     pl = [asset_x, asset_y]
+        #     rnd = Random().random()
+        #
+        #     self.assertEqual(Scheduler.shuffle_if_need(pl, index=0, old_index=0, repeated=2, shuffle_rnd=lambda: rnd), 0)
+        #     shuffle(pl_orig, lambda: rnd)
+        #
+        #     self.assertEqual(pl, pl_orig)
 
-    # def my_shuffle(assets):
-    #     rnd = Random.random()
-    #
-    #     def inner(a, b):
-    #         shuffle(assets, rnd)
-    #         shuffle(a, rnd)
-    #
-    #     return inner
-    #
-    # def test_get_next_asset_on_empty_assets_should_return_none(self):
-    #     self.assertEqual(self.scheduler.get_next_asset(), None)
-    #
-    # def test_get_next_asset_on_one_inactive_should_return_none(self):
-    #     assets_helper.create(self.conn, asset_x_inactive)
-    #     self.assertEqual(self.scheduler.get_next_asset(), None)
-    #
-    # def test_get_next_asset_all_inactive_should_return_none(self):
-    #     assets_helper.create_multiple(self.conn, [asset_x_inactive, asset_y_inactive])
-    #     self.assertEqual(self.scheduler.get_next_asset(), None)
-    #     self.assertEqual(self.scheduler.get_next_asset(), None)
-    #
-    # def test_get_next_asset_should_return_all_active(self):
-    #     settings['shuffle_playlist'] = False
-    #     assets_helper.create_multiple(self.conn, [asset_x, asset_x_inactive, asset_y, asset_y_inactive])
-    #
-    #     # first loop
-    #     self.assertEqual(self.scheduler.get_next_asset(), asset_y)
-    #     self.assertEqual(self.scheduler.get_next_asset(), asset_x)
-    #
-    #     # second loop
-    #     self.assertEqual(self.scheduler.get_next_asset(), asset_y)
-    #     self.assertEqual(self.scheduler.get_next_asset(), asset_x)
-    #
-    # def test_get_next_asset_should_shuffle_after_MAX_REPEAT(self):
-    #     settings['shuffle_playlist'] = True
-    #     viewer.SHUFFLE_MAX_REPEAT = 1
-    #     pl = [asset_x, asset_x_inactive, asset_y, asset_y_inactive]
-    #
-    #     assets_helper.create_multiple(self.conn, pl)
-    #     self.scheduler.update_pl()
-    #
-    #     def raise_error(a, b):
-    #         raise Exception('Not now, bro')
-    #
-    #     viewer.shuffle = raise_error
-    #
-    #     # first loop
-    #     self.assertEqual(self.scheduler.get_next_asset(), asset_y)
-    #     self.assertEqual(self.scheduler.get_next_asset(), asset_x)
-    #
-    #     # second loop
-    #     viewer.shuffle = self.my_shuffle(pl)
-    #
-    #     real_pl = filter(assets_helper.is_active, pl)
-    #
-    #     self.assertEqual(self.scheduler.get_next_asset(), real_pl[0])
-    #     self.assertEqual(self.scheduler.get_next_asset(), real_pl[1])
+        # def my_shuffle(assets):
+        #     rnd = Random.random()
+        #
+        #     def inner(a, b):
+        #         shuffle(assets, rnd)
+        #         shuffle(a, rnd)
+        #
+        #     return inner
+        #
+        # def test_get_next_asset_on_empty_assets_should_return_none(self):
+        #     self.assertEqual(self.scheduler.get_next_asset(), None)
+        #
+        # def test_get_next_asset_on_one_inactive_should_return_none(self):
+        #     assets_helper.create(self.conn, asset_x_inactive)
+        #     self.assertEqual(self.scheduler.get_next_asset(), None)
+        #
+        # def test_get_next_asset_all_inactive_should_return_none(self):
+        #     assets_helper.create_multiple(self.conn, [asset_x_inactive, asset_y_inactive])
+        #     self.assertEqual(self.scheduler.get_next_asset(), None)
+        #     self.assertEqual(self.scheduler.get_next_asset(), None)
+        #
+        # def test_get_next_asset_should_return_all_active(self):
+        #     settings['shuffle_playlist'] = False
+        #     assets_helper.create_multiple(self.conn, [asset_x, asset_x_inactive, asset_y, asset_y_inactive])
+        #
+        #     # first loop
+        #     self.assertEqual(self.scheduler.get_next_asset(), asset_y)
+        #     self.assertEqual(self.scheduler.get_next_asset(), asset_x)
+        #
+        #     # second loop
+        #     self.assertEqual(self.scheduler.get_next_asset(), asset_y)
+        #     self.assertEqual(self.scheduler.get_next_asset(), asset_x)
+        #
+        # def test_get_next_asset_should_shuffle_after_MAX_REPEAT(self):
+        #     settings['shuffle_playlist'] = True
+        #     viewer.SHUFFLE_MAX_REPEAT = 1
+        #     pl = [asset_x, asset_x_inactive, asset_y, asset_y_inactive]
+        #
+        #     assets_helper.create_multiple(self.conn, pl)
+        #     self.scheduler.update_pl()
+        #
+        #     def raise_error(a, b):
+        #         raise Exception('Not now, bro')
+        #
+        #     viewer.shuffle = raise_error
+        #
+        #     # first loop
+        #     self.assertEqual(self.scheduler.get_next_asset(), asset_y)
+        #     self.assertEqual(self.scheduler.get_next_asset(), asset_x)
+        #
+        #     # second loop
+        #     viewer.shuffle = self.my_shuffle(pl)
+        #
+        #     real_pl = filter(assets_helper.is_active, pl)
+        #
+        #     self.assertEqual(self.scheduler.get_next_asset(), real_pl[0])
+        #     self.assertEqual(self.scheduler.get_next_asset(), real_pl[1])
