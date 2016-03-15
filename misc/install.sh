fe69d9f8cb2f7adc9daefdaee5830a84be5fd82a
over <overln2@gmail.com>
2016-03-15 15:44:34 +0600

    fix

--- misc/install.sh
+++ misc/install.sh
@@ -2,7 +2,7 @@
 sudo easy_install pip
 sudo pip install ansible
 
-ansible localhost -m git -a "repo=git://github.com/wireload/screenly-ose.git dest=/home/pi/screenly version=master" || exit
+ansible localhost -m git -a "repo=git://github.com/over64/screenly-ose.git dest=/home/pi/screenly version=master" || exit
 cd /home/pi/screenly/misc/ansible
 ansible-playbook system.yml || exit
 ansible-playbook screenly.yml || exit
